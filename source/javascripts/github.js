ecce81c36439f8b617520d88b4e5153c0a7d44db
Fernando Mayer <fernandomayer@gmail.com>
2012-08-06 15:55:41 -0300

    license in footer, software page, fixed hithub.js

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -2,23 +2,33 @@ var github = (function(){
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
+      // fiz algumas modificacoes de acordo com
+      // https://gist.github.com/2925425#file_github.js.diff para fazer
+      // o pluging funcionar e nao ficar Status updating...
+
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+	// fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+	fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
+          // url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
+	  url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
         , type: 'jsonp'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
+	    if (!data || !data.data) { return; }
+	    for (var i = 0; i < data.data.length; i++) {
+		if (options.skip_forks && data.data[i].fork) { continue; }
+		repos.push(data.data[i]);
+          // if (!data || !data.repositories) { return; }
+          // for (var i = 0; i < data.repositories.length; i++) {
+          //   if (options.skip_forks && data.repositories[i].fork) { continue; }
+          //   repos.push(data.repositories[i]);
           }
           repos.sort(function(a, b) {
             var aDate = new Date(a.pushed_at).valueOf(),
---
+++ source/software/index.markdown
@@ -0,0 +1,26 @@
+---
+layout: page
+comments: false
+sharing: true
+footer: true
+---
+
+# Open-source software projects
+
+All projects are available from my GitHub repository at [https://github.com/fernandomayer](https://github.com/fernandomayer)
+
+## `R` packages
+
+* [FishMaps](https://github.com/fernandomayer/FishMaps#fishmaps-02-0):   Proportional symbol mapping for fishery data in batch mode.
+
+* FishMaps2: A `lattice` version of FishMaps. Under development, available soon.
+
+* Regrans: A "segmented" regression approach for morphometric data. Under development, available soon.
+
+## Configuration files
+
+These are not really software, but can be usefull
+
+* [R-config-files](https://github.com/fernandomayer/R-config-files): My `R` configuration files.
+
+* [emacs-files](https://github.com/fernandomayer/emacs-files): My Emacs configuration files.
